438980457fce6f79685a3a0f683fa29777e9d8b3
yashk04 <yashkasat04@gmail.com>
2019-11-17 15:11:32 +0530

    first commit

--- simplesocial/simplesocial/settings.py
+++ simplesocial/simplesocial/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar',
     'bootstrap4',
     'accounts',
     'posts',
@@ -52,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'simplesocial.urls'
--- simplesocial/simplesocial/urls.py
+++ simplesocial/simplesocial/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
-from django.conf import settings
 from . import views
 
 urlpatterns = [
@@ -29,9 +28,3 @@ urlpatterns = [
     url(r'^posts/',include('posts.urls',namespace='posts')),
     url(r'^groups/',include('groups.urls',namespace='groups')),
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        url(r'^__debug__/',include(debug_toolbar.urls)),
-    ] + urlpatterns
